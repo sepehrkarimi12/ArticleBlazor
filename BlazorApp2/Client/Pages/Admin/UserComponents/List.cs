36099dbace18c47adc50e9a35341a54853ffeed2
Sepehr Karimi <s.karimi@saadat.local>
2022-08-06 16:51:43 +0430

    created list page in admin section

---
+++ BlazorApp2/Client/Pages/Admin/UserComponents/List.cs
@@ -0,0 +1,63 @@
+﻿using BlazorApp2.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Client.Pages.Admin.UserComponents
+{
+    public partial class List
+    {
+        public List<User> Users;
+        public string Message = null;
+        public bool ShowMessage = false;
+        protected async override Task OnInitializedAsync()
+        {
+            var result = await userRepository.GetAllUsers();
+            ShowMessage = true;
+            if (result.Success)
+            {
+                Message = $"تعداد رکورد های دریافتی برابر با {result.Response.Count}";
+                if (result.Response != null && result.Response.Count > 0)
+                {
+                    Users = result.Response;
+                }
+                else
+                {
+                    Users = new List<User>();
+                }
+            }
+            else
+            {
+                Message = "دریافت اطلاعات با خطا مواجه شد";
+            }
+        }
+
+        private void EditUser(long Id)
+        {
+            navigation.NavigateTo($"/admin/users/edit/{Id}");
+        }
+
+        private async Task DeleteUser(User user)
+        {
+            var result = await userRepository.DeleteUser(user);
+            ShowMessage = true;
+            if (result.Success)
+            {
+                if (result.Response)
+                {
+                    Message = "کاربر مورد نظر حذف شد";
+                    Users.Remove(user);
+                }
+                else
+                {
+                    Message = "انجام عملیات با خطا مواجه شد";
+                }
+            }
+            else
+            {
+                Message = "درخواست شما با خطا مواجه شد";
+            }
+        }
+    }
+}
