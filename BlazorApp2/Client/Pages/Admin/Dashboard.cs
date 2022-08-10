bf51a63463d0f7129ae6baf45737e7a85553317b
Sepehr Karimi <s.karimi@saadat.local>
2022-08-10 09:21:46 +0430

    created Dashboard page, the url is admin

---
+++ BlazorApp2/Client/Pages/Admin/Dashboard.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Client.Pages.Admin
+{
+    public partial class Dashboard
+    {
+        private AuthenticationState authState { get; set; }
+        private List<Claim> claims { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(1000);
+            this.authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            this.claims = this.authState.User.Claims.ToList();
+        }
+    }
+}
